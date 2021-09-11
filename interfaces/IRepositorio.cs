0266a8c7e8f1f5f0f0ea6a9fa7a44f6e4cca07f3
Matheus Yuri <matheusyurirs@gmail.com>
2021-09-11 18:35:06 -0300

    App de estudo para cadastro em Csharp

---
+++ interfaces/IRepositorio.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Series.interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
